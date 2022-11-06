f201a589166f05851650cbf77d3bebdb2313eb91
Amm0n1te <103222407+Amm0n1te@users.noreply.github.com>
2022-11-05 18:26:27 -0700

    added parallax planets

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -4,40 +4,54 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject binding;
     float lastFrame = 0;
     float thisFrame;
     private bool started = false;
-    public float parallaxSpeed = 10f;   //making this variable public breaks the parallax for some reason ???
-    float halfScreenLength = 25;
+    float parallaxSpeed;   //making this variable public breaks the parallax for some reason ???
+    float parallaxSpeedBase = 35f;
+    float halfScreenLength = 21;
+    
 
 
     void Start()
     {
-        
+        //Debug.Log(transform.localScale.x);
+        parallaxSpeed = parallaxSpeedBase*(transform.localScale.x)/8f;
+        //parallaxSpeed = parallaxSpeedBase;
+        //Debug.Log(parallaxSpeed);
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        thisFrame = player.transform.position.x;
+        thisFrame = binding.transform.position.x;
         if (!started) {
             lastFrame = thisFrame;
             started = true;
         }
         transform.Translate(new Vector3(-(thisFrame-lastFrame) * Time.deltaTime * parallaxSpeed, 0f, 0f), Space.World);
-        lastFrame = player.transform.position.x;
-        //Debug.Log(started);
+        lastFrame = binding.transform.position.x;
 
-        //Debug.Log(transform.position.x.ToString()+" < "+ (player.transform.position.x-halfScreenLength).ToString());
-        Debug.Log(transform.position.x.ToString()+" vs player "+player.transform.position.x.ToString());
-        if (transform.position.x > player.transform.position.x+halfScreenLength) {
+
+        if (transform.position.x > binding.transform.position.x+halfScreenLength) {
             transform.Translate(new Vector3(-2*halfScreenLength, 0, 0));
-            Debug.Log("planet teleported left");
+            transform.position = new Vector3(transform.position.x, Random.Range(-5f, 5f), 0);
+            float gotRange = Random.Range(0.5f, 13f);
+            transform.localScale = new Vector3(gotRange,gotRange,gotRange);
+            parallaxSpeed = parallaxSpeedBase*(transform.localScale.x)/8f;
+            //Debug.Log(parallaxSpeed);
+            //Debug.Log("planet teleported left");
         }
-        else if (transform.position.x < player.transform.position.x-halfScreenLength) {
+        else if (transform.position.x < binding.transform.position.x-halfScreenLength) {
             transform.Translate(new Vector3(2*halfScreenLength, 0, 0));
-            Debug.Log("planet teleported right");
+            transform.position = new Vector3(transform.position.x, Random.Range(-5f, 5f), 0);
+            float gotRange = Random.Range(0.5f, 13f);
+            transform.localScale = new Vector3(gotRange,gotRange,gotRange);
+            parallaxSpeed = parallaxSpeedBase*(transform.localScale.x)/8f;
+            //Debug.Log(parallaxSpeed);
+            //Debug.Log("planet teleported right");
         }
 
     }
